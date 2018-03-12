015325bf5964aa38973367c4684f2929338cda8c
clwaddel <30387984+clwaddel@users.noreply.github.com>
2018-03-12 17:24:44 -0400

    first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Simple script to create the Makefile and build
+
+# export PATH="$PATH:/usr/local/cuda/bin/"
+
+make distclean || echo clean
+
+rm -f Makefile.in
+rm -f config.status
+./autogen.sh || echo done
+
+# CFLAGS="-O2" ./configure
+extracflags="-march=native -std=c++11 -pthread -fno-strict-aliasing"
+CUDA_CFLAGS="-O3 -std=c++11 -Xcompiler -fno-strict-aliasing,-Wall -D_FORCE_INLINES"
+
+./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
+
+make -j4
